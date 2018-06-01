298b40dabe7e82a516e1459e87047ffe912cd27b
wasifmiraz <wasifmiraz@mail.com>
2018-06-01 12:44:53 +0600

    view part done

---
+++ app.js
@@ -0,0 +1,16 @@
+var app = new Vue({
+	el: "#root",
+	data: {
+		showingAddModal: false,
+		showingEditModal: false,
+		showingDeleteModal: false
+	},
+
+	mounted: function(){
+		console.log("mounted");
+	},
+
+	methods: {
+
+	}
+});
